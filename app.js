fba76dea29f622f82c82150285430d7fc6f1705d
sarthak gaur <sarthakgaur.sg@gmail.com>
2020-02-06 01:22:01 +0530

    Instructions page completed

--- app.js
+++ app.js
@@ -1476,11 +1476,10 @@ app.get('/:team_id/report', function(req, res){
 //===============
 
 app.get("/directions",function(req,res){
-  Session.find({status:1},function(err,ses){
+  Session.findOne({status:1},function(err,ses){
     res.render("directions",{ses:ses});
-
+    console.log("Instructions: done:"+ ses.sprinTime);
   });
-  console.log("Instructions: done");
 });
 
 //===============
